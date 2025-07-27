b6c2d815ea81da789de38670256f1200498b8757
simo-elk <simo_7@outlook.com>
2025-07-27 17:09:20 +0200

    Update main.js

--- code/main.js
+++ code/main.js
@@ -1,2 +1,3 @@
 console.log('Running the content of main.js');
 console.log("Environment variable MY_VARIABLE:", process.env.MY_VARIABLE);
+console.log("The centralized variables is:", process.env.CENTRALIZED_VARIABLE)
